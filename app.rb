4c3483fb035626abe8c53f34c89bb822c3a0ac0e
jimon93 <zeroga12@gmail.com>
2013-09-05 13:51:28 +0900

    option指定をかっこよく

--- app.rb
+++ app.rb
@@ -23,18 +23,49 @@ end #}}}
 require 'optparse'
 require 'fssm'
 
-pattern = ARGV[0]
-command = ARGV[1..-1].join(" ")
+class MyOptionParser
+  def initialize
+    @option = {:pattern => "**/*"}
+    @parser = OptionParser.new
+
+    @parser.on("-p", "--pattern PATTERN"){ |v| @option[:pattern] = v }
+  end
+
+  def parse
+    argv = @parser.parse(ARGV)
+    [@option, argv]
+  end
+end
+
+class Commandor
+  def initialize(option, argv)
+    @command = argv.join(" ")
+  end
+
+  def execute(base, file)
+    command = create_command(base, file)
+    puts `#{command}`
+  end
+
+  private
+  def create_command(base, file)
+    dict = { "base" => base, "file" => file }
+    @command.gsub(/%(file|base)/){ dict[$1] }
+  end
+end
+
+
+option, argv = MyOptionParser.new.parse
+pattern = option[:pattern]
+commandor = Commandor.new(option, argv)
 
 FSSM.monitor('.', pattern) do
   update do |base, file|
-    path = "#{base}/#{file}"
-    puts "UPDATE: #{path}"
-    puts `#{command} #{path}`
+    puts "UPDATE: #{base}/#{file}"
+    commandor.execute(base, file)
   end
   create do |base, file|
-    path = "#{base}/#{file}"
-    puts "CREATE: #{path}"
-    puts `#{command} #{path}`
+    puts "CREATE: #{base}/#{file}"
+    commandor.execute(base, file)
   end
 end
