cf48ab3ca9a208451e524fa8dca9529f90d12994
jimon93 <zeroga12@gmail.com>
2013-09-06 14:24:37 +0900

    いろいろかえた

--- app.rb
+++ app.rb
@@ -1,22 +1,26 @@
 #!/usr/local/bin/ruby
+Version = "0.0.1"
 require 'optparse'
 require 'fssm'
 
 class Main
   def initialize
-    options = MyOptionParser.new.parse
-    @pattern = Pattern.new(options)
-    @command = Command.new(options)
+    @options =  MyOptionParser.new.parse
+    @watch_path = WatchPath.new(@options)
+    @pattern = Pattern.new(@options)
   end
 
   def start
-    cmd = @command
-    FSSM.monitor('.', @pattern) do
+    options = @options
+    FSSM.monitor(@watch_path, @pattern) do
       update do |base, file|
-        cmd.execute(base, file)
+        UpdateCommand.new(options, base, file).execute
       end
       create do |base, file|
-        cmd.execute(base, file)
+        CreateCommand.new(options, base, file).execute
+      end
+      remove do |base, file|
+        RemoveCommand.new(options, base, file).execute
       end
     end
   end
@@ -24,39 +28,90 @@ end
 
 class MyOptionParser
   def initialize
-    @options = {:pattern => "**/*"}
+    @options = {:path => ".", :pattern => "**/*"}
     @parser = OptionParser.new
 
-    @parser.on("-p", "--pattern PATTERN"){ |v| @options[:pattern] = v }
+    desc = "one or more glob pattern (default: '**/*')"
+    @parser.on("-p", "--pattern PATTERN", desc){ |v| @options[:pattern] = v }
+
+    desc =  "the path to watch (default: '.')"
+    @parser.on("-w", "--watch-path PATH", desc){ |v| @options[:path] = v }
+
+    desc = "when a file is create, use a command"
+    @parser.on('-c', "--create COMMAND", desc){ |v| @options[:create] = v }
+
+    desc = "when a file is update, use a command"
+    @parser.on('-u', "--update COMMAND", desc){ |v| @options[:update] = v }
+
+    desc = "when a file is remove, use a command"
+    @parser.on('-r', "--remove COMMAND", desc){ |v| @options[:remove] = v }
   end
 
   def parse
-    @options[:argv] = @parser.parse(ARGV)
+    @options[:argv] = @parser.order(ARGV)
     @options
   end
 end
 
-class Pattern < String
+class WatchPath < String
   def initialize(options)
-    super options[:pattern]
+    super options[:path]
+  end
+end
+
+class Pattern < Array
+  def initialize(options)
+    super options[:pattern].split(" ")
   end
 end
 
 class Command
-  def initialize(options)
-    @base_command = options[:argv].join(" ") || ""
+  def initialize(options, base, file)
+    @options, @base, @file = options, base, file
   end
 
-  def execute(base, file)
-    dict = { "base" => base, "file" => file }
-    command = @base_command.gsub(/%(file|base)/){ dict[$1] }
-
-    puts "command: `#{command}`"
-    puts `#{command}`
+  def execute
+    puts "#{self.class.name}: `#{extend_command}`"
+    puts `#{extend_command}`
   end
 
-  def to_s
-    @base_command
+  protected
+  def base_command
+    raise NotImplementedError
+  end
+
+  private
+  def extend_command
+    @_extend_command ||= base_command.gsub(/[$%](file|base|dir)/){ dict[$1] }
+  end
+
+  def dict
+    @_dict ||= {
+      "base" => base,
+      "file" => file,
+      "dir"  => File.dirname(file)
+    }
+  end
+end
+
+class UpdateCommand < Command
+  protected
+  def base_command
+    @options[:update] || @options[:argv].join(" ") || ""
+  end
+end
+
+class CreateCommand < Command
+  protected
+  def base_command
+    @options[:create] || @options[:argv].join(" ") || ""
+  end
+end
+
+class RemoveCommand < Command
+  protected
+  def base_command
+    @options[:remove] ||  ""
   end
 end
 
